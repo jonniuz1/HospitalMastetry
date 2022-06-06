22bdecc3fdcc01b67ec8490f12119e3097fdcb1b
jonniuz1 <joevega3574@gmail.com>
2022-06-06 23:41:45 +0500

    Added time to base.html

--- hospital/static/js/script.js
+++ hospital/static/js/script.js
@@ -9,3 +9,9 @@ $(document).ready(function () {
         $("#no-data").text("No patient found");
     }
 });
+
+// 2) Time running at real time
+setInterval(function () {
+    var date = new Date();
+    $("#clock").html((date.getHours() < 10 ? '0' : '') + date.getHours() + ":" + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes() + ":" + (date.getSeconds() < 10 ? '0' : '') + date.getSeconds());
+}, 500);
